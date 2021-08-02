fcd8d4fc5d0b9b1dc7d51b8c0fd4ae840547b7ec
Leo Baleon <leonardo.baleon@gmail.com>
2021-08-02 00:18:31 -0700

    Implemented the functions necessary to display all tickets or a single ticket at a time, and the necessary helper functions

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 from requests import get
 import os
+import math
 
 # original url for making GET request
 ORIGINALURL = 'https://zccsonmac.zendesk.com/api/v2/tickets.json'
@@ -19,18 +20,87 @@ PASSWORD = os.environ['PASS']
 payload = get(ORIGINALURL, auth=(USERNAME, PASSWORD))
 jsonResponse = payload.json()
 
-# this form of indexing filters the JSON data to the desired values
-    # print(jsonResponse["tickets"][0]["url"])
+def display(ticketNumber):
+    print("Ticket with subject \'" + \
+            jsonResponse['tickets'][ticketNumber]['subject'] + \
+            "\' opened by " + \
+            str(jsonResponse['tickets'][ticketNumber]['requester_id']) + \
+            " on " + \
+            jsonResponse['tickets'][ticketNumber]['created_at'])
+
+def displayTickets(start, num):
+    # to return the number of tickets displayed in this function call
+    count = 0
+
+    # for each ticket in the specifed number of tickets to display
+    for i in range(start, start+num):
+        display(i)
+
+        # keep count
+        count += 1
+
+    return count
 
 def allTickets():
-    print("Temporary")
+    # max tickets per page
+    max = 25
+
+    # total tickets
+    total = jsonResponse['count']
+
+    # remaining number of tickets to display
+    remaining = total
+
+    # current page
+    page = 1
+
+    # starting ticket
+    start = 0
+
+    # count returned
+    count = 0
+
+    # check if there are more than 25 tickets (for pagination)
+    if remaining > max:
+        # to keep getting more pages as long as the user desires
+        while remaining > 0:
+            # display up to 25 tickets at a time. the ternary operator will check
+            # if the remaining tickets is greater than 25, and will set a max of 25
+            # if true, or simply send in the remaining tickets otherwise
+            count = displayTickets(start, max if remaining > max else remaining)
+
+            # update values after each call
+            remaining -= count
+            start += count
+            
+            # print the current page and total page count
+            print("Page " + str(page) + " of " + str(math.ceil(total / 25)))
+
+            if remaining > 0:
+                userInput = input("\nPress 1 to view the next page, anything else to return to the previous menu:\n")
+
+            if userInput != '1' or remaining == 0:
+                break
+            else:
+                page += 1
+                
+    else:
+        displayTickets(start, remaining)
 
 def singleTicket():
-    ticketNumber = input("Enter a ticket number:\n")
+    # total tickets
+    total = jsonResponse['count']
+
+    ticketNumber = int(input("Enter a ticket number:\n"))
+
+    # validate ticket number
+    while ticketNumber >= total or ticketNumber < 0:
+        ticketNumber = int(input("Invalid ticket number\nEnter a ticket number:\n"))
+
+    display(ticketNumber)
+
 
 def menu(userInput):
-    
-
     while userInput != 'quit':
         userInput = input("\n\tSelect view options:\n" \
                         "\t* Press 1 to view all tickets\n" \
@@ -57,8 +127,9 @@ def main():
 
     if userInput == "menu":
         menu(userInput)
-    else:
-        return 0
+
+    print("Thanks for using the viewer. Goodbye.")
+    return 0
 
 if __name__ == "__main__": main()
 
